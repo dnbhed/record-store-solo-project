6c38721a65c1510d47397cb18cd06d781a03d22d
Tam O'Donnell <thomashodonnell@gmail.com>
2019-04-26 13:15:41 +0100

    add inventory level method - returns string

--- models/desk.rb
+++ models/desk.rb
@@ -7,7 +7,14 @@ class Desk
   end
 
   def check_stock_by_id(record_id)
-    return Record.find(record_id).id
+    result = Record.find(record_id).id
+    if result > 10
+      return "High"
+    elsif result > 4
+      return "Medium"
+    else
+      return "Low"
+    end
   end
 
 end
