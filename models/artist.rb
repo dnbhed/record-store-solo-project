fbd0474d95ea1af6c4dde7729a02520854279a04
Tam O'Donnell <thomashodonnell@gmail.com>
2019-04-26 10:51:12 +0100

    test save method success

--- models/artist.rb
+++ models/artist.rb
@@ -7,6 +7,19 @@ class Artist
     @name = options['name']
   end
 
-  
+  def save()
+    sql = "INSERT INTO artists
+    (
+      name
+    ) VALUES
+    (
+      $1
+    )
+      RETURNING id"
+    values = [@name]
+    results = SqlRunner.run(sql, values)
+    @id = results.first()['id'].to_i
+
+  end
 
 end
