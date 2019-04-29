a2c91fb042457fee3092ac88f6ff0792ca30c757
Tam O'Donnell <thomashodonnell@gmail.com>
2019-04-29 10:07:17 +0100

    finish restful artists

--- controllers/artists_controller.rb
+++ controllers/artists_controller.rb
@@ -13,3 +13,24 @@ get '/artists/new' do
   @artists = Artist.all()
   erb(:"artists/new")
 end
+
+post '/artists' do
+  Artist.new(params).save()
+  redirect to '/artists'
+end
+
+get '/artists/:id' do
+  @artist = Artist.find(params['id'])
+  erb(:"artists/show")
+end
+
+get '/artists/:id/edit' do
+  @artist = Artist.find(params['id'])
+  erb(:"artists/edit")
+end
+
+post '/artists/:id' do
+  artist = Artist.new(params)
+  artist.update()
+  redirect to "/artists/#{params['id']}"
+end
--- models/artist.rb
+++ models/artist.rb
@@ -2,7 +2,8 @@ require_relative('../db/sql_runner')
 
 class Artist
 
-  attr_reader :id, :name
+  attr_reader :id
+  attr_accessor :name
 
   def initialize( options )
     @id = options['id'].to_i if options['id']
@@ -21,6 +22,15 @@ class Artist
     values = [@name]
     results = SqlRunner.run(sql, values)
     @id = results.first()['id'].to_i
+  end
+
+  def update()
+    sql = "UPDATE artists
+    SET
+    name = $1
+    WHERE id = $2;"
+    values = [@name, @id]
+    SqlRunner.run(sql, values)
   end
 
   def self.find(id)
