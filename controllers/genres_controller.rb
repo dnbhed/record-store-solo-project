a64b94d52010a8c72749a778f7afc99bf2b3d663
Tam O'Donnell <thomashodonnell@gmail.com>
2019-04-30 15:15:16 +0100

    create restful files for new classes. finish restful for genres

---
+++ controllers/genres_controller.rb
@@ -0,0 +1,46 @@
+require('sinatra')
+require('sinatra/contrib/all')
+require_relative('../models/genre.rb')
+also_reload('../models/*')
+
+get '/genres' do
+  @genres = Genre.all()
+  erb(:"genres/index")
+end
+
+
+get '/genres/new' do
+  @genres = Genre.all()
+  erb(:"genres/new")
+end
+
+post '/genres' do
+  Genre.new(params).save()
+  redirect to '/genres'
+end
+
+get '/genres/:id' do
+  @genre = Genre.find(params['id'])
+  erb(:"genres/show")
+end
+
+get '/genres/:id/edit' do
+  @genre = Genre.find(params['id'])
+  erb(:"genres/edit")
+end
+
+post '/genres/:id' do
+  genre = Genre.new(params)
+  genre.update()
+  redirect to "/genres/#{params['id']}"
+end
+
+post '/genres/:id/delete' do
+  genre = Genre.find(params['id'])
+  tracks = genre.tracks()
+  for track in tracks
+    track.delete
+  end
+  genre.delete()
+  redirect to '/genres'
+end
--- desk.rb
+++ desk.rb
@@ -2,6 +2,7 @@ require('sinatra')
 require('sinatra/contrib/all')
 require_relative('controllers/artists_controller.rb')
 require_relative('controllers/records_controller.rb')
+require_relative('controllers/genres_controller.rb')
 also_reload('models/*')
 
 get '/' do
