a701a6ee51c6d60d432bb77f16f83f72d7b18e87
Tam O'Donnell <thomashodonnell@gmail.com>
2019-04-30 10:47:47 +0100

    change record seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,15 @@
 require_relative('../models/artist')
 require_relative('../models/record')
-require_relative('../models/desk')
+require_relative('../models/genre')
+require_relative('../models/track_record')
+require_relative('../models/track')
+
 require('pry-byebug')
 
 Record.delete_all()
 Artist.delete_all()
 
-desk = Desk.new("1")
+
 
 artist1 = Artist.new({
   'name' => 'Matrix & Futurebound'})
@@ -21,22 +24,30 @@ artist3.save()
 record1 = Record.new({
   'title' => 'Back to Basics',
   'stock' => '3',
-  'artist_id' => artist2.id
+  'buy_price' => '5',
+  'sell_price' => '5'
+  'vinyl_wav' => 'true'
   })
 record2 = Record.new({
   'title' => 'High Society',
   'stock' => '10',
-  'artist_id' => artist3.id
+  'buy_price' => '5',
+  'sell_price' => '5'
+  'vinyl_wav' => 'true'
   })
 record3 = Record.new({
   'title' => 'Low Society',
   'stock' => '11',
-  'artist_id' => artist3.id
+  'buy_price' => '5',
+  'sell_price' => '5'
+  'vinyl_wav' => 'true'
   })
 record4 = Record.new({
   'title' => 'Magnetic Eyes',
   'stock' => '5',
-  'artist_id' => artist1.id
+  'buy_price' => '5',
+  'sell_price' => '5'
+  'vinyl_wav' => 'true'
   })
 
 record1.save()
