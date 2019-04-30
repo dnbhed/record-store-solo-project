20b9eef98338e800cd08b527af5ff21f9141afab
Tam O'Donnell <thomashodonnell@gmail.com>
2019-04-30 10:29:45 +0100

    add genres, tracks, tracks_records tables

--- db/record_store.sql
+++ db/record_store.sql
@@ -1,6 +1,16 @@
+DROP TABLE tracks_records;
+DROP TABLE tracks;
 DROP TABLE records;
 DROP TABLE artists;
+DROP TABLE genres;
 
+
+CREATE TABLE genres
+(
+  id SERIAL8 primary key,
+  name VARCHAR(255),
+  bpm INT2
+);
 
 CREATE TABLE artists
 (
@@ -13,5 +23,23 @@ CREATE TABLE records
   id SERIAL8 primary key,
   title VARCHAR(255) not null,
   stock INT4,
-  artist_id INT8 references artists(id) ON DELETE SET NULL
+
+);
+
+CREATE TABLE tracks
+(
+  id SERIAL8 primary key,
+  title VARCHAR(255) not null,
+  artist_id INT8 references artists(id) ON DELETE SET NULL,
+  genre_id INT8 references genres(id) ON DELETE SET NULL,
+  label VARCHAR(255) not null,
+  key VARCHAR(2) not null
+);
+
+CREATE TABLE tracks_records
+(
+  id SERIAL8 primary key,
+  track_id INT8 references tracks(id) ON DELETE SET NULL,
+  record_id INT8 references records(id) ON DELETE SET NULL,
+  track_number INT2 not null
 );
