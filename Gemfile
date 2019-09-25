e925f8f33375201534c3bdaf836facdb2d6c50e4
TamHOD <thomashodonnell@gmail.com>
2019-09-25 10:11:20 +0100

    setup db for development

--- Gemfile
+++ Gemfile
@@ -3,7 +3,7 @@
 
 
 source 'https://rubygems.org'
-ruby '2.4.1'
+ruby '2.6.1'
 gem 'sinatra'
 gem 'sinatra-contrib'
 gem 'mustermann'
--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -4,7 +4,7 @@ class SqlRunner
 
   def self.run( sql, values = [] )
     begin
-      db = PG.connect({ dbname: 'd1j4m54eik8k4l', host: 'ec2-174-129-208-118.compute-1.amazonaws.com', user: 'knhrxndktlsqkx', password: '64247f23c4330f871c9dc862ab2aff05a83e7df44c07a88050e12099c7e354d0' })
+      db = PG.connect({ dbname: 'recordstore', host: 'localhost' })
       db.prepare("query", sql)
       result = db.exec_prepared( "query", values )
     ensure
