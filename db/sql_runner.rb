2f70868659eff63c43861ce8d61161741c204221
TamHOD <thomashodonnell@gmail.com>
2019-09-25 10:24:21 +0100

    change css

--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -4,7 +4,7 @@ class SqlRunner
 
   def self.run( sql, values = [] )
     begin
-      db = PG.connect({ dbname: 'recordstore', host: 'localhost' })
+      db = PG.connect({ dbname: 'record_store', host: 'localhost' })
       db.prepare("query", sql)
       result = db.exec_prepared( "query", values )
     ensure
